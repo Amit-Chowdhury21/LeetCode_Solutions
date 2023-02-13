d6c995c775739a375ac63e1c3510006a9cecdfac
Amit_Chowdhury21 <2022psp0004@iitjammu.ac.in>
2023-02-13 23:10:46 +0530

    Update 0383.RansomNote.cpp

--- 0383.RansomNote.cpp
+++ 0383.RansomNote.cpp
@@ -1,11 +1,9 @@
 /*
 ************************************************************ 383. Ransom Note ************************************************************
 
+
 Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
-
 Each letter in magazine can only be used once in ransomNote.
-
- 
 
 Example 1:
 
