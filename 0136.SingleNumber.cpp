2ae01e8605f2fc2e6420b40fa0f575aadf857c6b
Amit_Chowdhury21 <2022psp0004@iitjammu.ac.in>
2023-02-24 11:01:17 +0530

    Update 0136.SingleNumber.cpp

--- 0136.SingleNumber.cpp
+++ 0136.SingleNumber.cpp
@@ -16,12 +16,11 @@ Input: nums = [4,1,2,1,2]
 Output: 4
 Example 3:
 
-Input: nums = [1]
+Input: 
+nums = [1]
 Output: 1
  
-
 Constraints:
-
 1 <= nums.length <= 3 * 104
 -3 * 104 <= nums[i] <= 3 * 104
 Each element in the array appears twice except for one element which appears only once.
