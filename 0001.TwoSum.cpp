37ce6e3001acd9e4bb7b28766ef11f937b7efdf5
Amit_Chowdhury21 <2022psp0004@iitjammu.ac.in>
2023-03-21 23:59:24 +0530

    Update 0001.TwoSum.cpp

--- 0001.TwoSum.cpp
+++ 0001.TwoSum.cpp
@@ -1,7 +1,7 @@
 /*
 ************************************************************ 1.Two Sum ************************************************************
 
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target. 
 
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
